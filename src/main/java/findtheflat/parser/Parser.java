101651cf10271292347dfcbeb958bddda7cb71c4
Denis <dstiven@i.ua>
2018-08-02 14:53:03 +0200

    Basic implementation of web sites parsing

--- src/main/java/findtheflat/parser/Parser.java
+++ src/main/java/findtheflat/parser/Parser.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
  */
 public class Parser
 {
-    public ArrayList<String> getUrlsFromOlx(String searchQuery)
+    public ArrayList<String> getUrlsFromOlxPl(String searchQuery)
     {
         ArrayList<String> urls = new ArrayList<>();
         Document doc = null;
@@ -41,4 +41,90 @@ public class Parser
         }
         return urls;
     }
+    public ArrayList<String> getUrlsFromGratkaPl(String searchQuery)
+    {
+        ArrayList<String> urls = new ArrayList<>();
+        Document doc = null;
+        Elements elements = null;
+        
+        try
+        {
+            doc = Jsoup.connect(searchQuery).get();
+            elements = doc.getElementsByAttributeValueContaining("id","offer");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        if(!elements.isEmpty())
+        {
+            for(Element element: elements)
+            {
+                String url = element.getElementsByTag("a").attr("href");
+                if(url != null && !url.isEmpty() && url.contains("gratka.pl"));
+                {
+                    urls.add(url);
+                }
+            }
+        }
+        return urls;
+    }
+    public ArrayList<String> getUrlsFromOtodomPl(String searchQuery)
+    {
+        ArrayList<String> urls = new ArrayList<>();
+        Document doc = null;
+        Elements elements = null;
+    
+        try
+        {
+            doc = Jsoup.connect(searchQuery).get();
+            elements = doc.getElementsByAttributeValueContaining("class","offer-item-details");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        if(!elements.isEmpty())
+        {
+            for(Element element: elements)
+            {
+                String url = element.getElementsByTag("a").attr("href");
+                if(url != null && !url.isEmpty() && url.contains("otodom.pl"))
+                {
+                    urls.add(url);
+                }
+            }
+        }
+        return urls;
+    }
+    public ArrayList<String> getUrlsFromGumtreePl(String searchQuery)
+    {
+        ArrayList<String> urls = new ArrayList<>();
+        Document doc = null;
+        Elements elements = null;
+        String url = "https://www.gumtree.pl";
+        
+        try
+        {
+            doc = Jsoup.connect(searchQuery).get();
+            elements = doc.getElementsByAttributeValue("class","result-link  ");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        if(!elements.isEmpty())
+        {
+            for(Element element: elements)
+            {
+                    url += element.getElementsByTag("a").attr("href");
+                    if(url != null && !url.isEmpty() )
+                    {
+                        urls.add(url);
+                    }
+            }
+        }
+        return urls;
+    }
 }
+
